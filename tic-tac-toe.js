69f9d7e9004582334d9c9b5b5bc27a9da91fcc82
SC-Dixon <shantay1234.sd@gmail.com>
2022-10-07 22:31:23 -0500

    Edited tic-tac-toe.js file so that an X or O iss added when a square is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,11 +6,37 @@ window.onload = function(){
 
     let sqDivs = board.getElementsByTagName("div");
 
-    [...sqDivs].forEach(function(elem) 
+    [...sqDivs].forEach(function(elem, index) 
     {
         elem.classList.add("square");
+        elem.addEventListener("click", () => {
+            squareClicked(index);
+        });
     });
 
+    let boardLayout = [0,0,0,0,0,0,0,0,0];
+
+    let playerX = 'X';
+    let playerO = 'O';
+    let currPlayer = 'X';
+    
+
+    function squareClicked(e){
+        boardLayout[e] = currPlayer;
+        if (currPlayer == 'X'){
+            sqDivs[e].textContent = playerX;
+            sqDivs[e].classList.add("X");
+            currPlayer = playerO;
+        }else{
+            sqDivs[e].textContent = playerO;
+            sqDivs[e].classList.add("O");
+            currPlayer = playerX;
+        }
+
+    }
+
+
+    
 
 }
 
